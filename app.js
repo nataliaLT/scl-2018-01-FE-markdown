bc20ffb7ef3ef6ba63e570ba4ae8c0d880553562
natalia <natalia.lucerot@gmail.com>
2018-08-30 14:56:09 -0500

    funcion para validar

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const ruta= path.resolve(path.join(process.cwd(), 'README.md'));
 const fetch = require('node-fetch');
 console.log(ruta)
 //const fetch = require('node-fetch');
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 fs = require('fs');
 
 fs.readFile(ruta, 'utf-8', function (err,data){
     if(err) throw err;
     {
-       console.log(err);
+       //console.log(err);
     }
     extraerLinks(data);
-   
 });
+
 
 //npm install --save marked
 const Marked = require('marked');
@@ -27,11 +28,7 @@ function extraerLinks(markdown) {
   const renderer = new Marked.Renderer();
 
   // Taken from https://github.com/markedjs/marked/issues/1279
-  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
 
-  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
-  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
-  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
 
   renderer.link = function(href, title, text) {
     links.push({
@@ -40,7 +37,7 @@ function extraerLinks(markdown) {
       title: title,
     });
   };
-  renderer.image = function(href, title, text) {
+ /* renderer.image = function(href, title, text) {
       // Remove image size at the end, e.g. ' =20%x50'
       href = href.replace(/ =\d*%?x\d*%?$/, '');
       links.push({
@@ -48,43 +45,23 @@ function extraerLinks(markdown) {
         text: text,
         title: title,
       });
-  };
+  };*/
   Marked(markdown, {renderer: renderer});
   validarLinks(links)
-  console.log(links)
+  //console.log(links)
   // return links;
 
 };
 
-/*//funcion para validar los links 
-function validarLinks(links){
-  let url = links.href;
-  console.log('holaa');
-  links.forEach(element=>{
-    const http = require('http');
-    fetch(http.get(url,(res)=>{
-      const {statusCode} = res;
-    })).then(response => response
-
-  ).then(data =>{
-    if(data.statusCode === 200){
-      console.log('ok')
-    }
-  }).catch(error =>{
-    console.log ('fail')
-  })
-  })
-  
-}
-*/
-function validarLinks(links) {
+/*function validarLinks(links) {
   links.forEach(element => {
     let url = element.href;
+    console.log(url)
     fetch(url).then(response => response
     ).then(data => {
-      console.log(data.url);
-      console.log(data.status);
-      console.log(data.statusText); 
+      //console.log(data.url);
+      //console.log(data.status);
+      //console.log(data.statusText); 
 
       if (data.status=='200'){
       }
@@ -92,4 +69,23 @@ function validarLinks(links) {
       console.error('ERROR > ' + error.status);
     });
   });
+}*/
+
+
+function validarLinks(links) {
+  links.forEach(element => {
+    let url = element.href;
+    console.log(url)
+  //obtener 
+  const xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState === 200 && xhttp.status === 200) {
+      console.log((url, xhttp.status));
+    } else if ( xhttp.readyState == 4 && xhttp.status != 200 ){
+     console.log((url, xhttp.status));
+    } 
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+  })
 }
